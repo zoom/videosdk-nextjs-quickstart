4b149c7d1fc9b6170298cdc528375736be9eda87
Ekaansh Arora <ekaansh68@gmail.com>
2024-06-11 19:10:07 +0530

    chore: clean up some code for the blog

--- src/components/Videocall.tsx
+++ src/components/Videocall.tsx
@@ -14,7 +14,7 @@ import { Button } from "./ui/button";
 const Videocall = (props: { slug: string; JWT: string }) => {
   const session = props.slug;
   const jwt = props.JWT;
-  const [inCall, setInCall] = useState(false);
+  const [inSession, setInSession] = useState(false);
   const client = useRef<typeof VideoClient>(ZoomVideo.createClient());
   const [isVideoMuted, setIsVideoMuted] = useState(
     !client.current.getCurrentUserInfo()?.bVideoOn
@@ -24,7 +24,7 @@ const Videocall = (props: { slug: string; JWT: string }) => {
   );
   const videoContainerRef = useRef<HTMLDivElement>(null);
 
-  const init = async () => {
+  const joinSession = async () => {
     await client.current.init("en-US", "Global", { patchJsMedia: true });
     client.current.on(
       "peer-video-state-change",
@@ -33,11 +33,7 @@ const Videocall = (props: { slug: string; JWT: string }) => {
     await client.current.join(session, jwt, userName).catch((e) => {
       console.log(e);
     });
-  };
-
-  const startCall = async () => {
-    await init();
-    setInCall(true);
+    setInSession(true);
     const mediaStream = client.current.getMediaStream();
     // @ts-expect-error https://stackoverflow.com/questions/7944460/detect-safari-browser/42189492#42189492
     window.safari
@@ -71,16 +67,15 @@ const Videocall = (props: { slug: string; JWT: string }) => {
     }
   };
 
-  const leaveCall = async () => {
+  const leaveSession = async () => {
     client.current.off(
       "peer-video-state-change",
       (payload: { action: "Start" | "Stop"; userId: number }) =>
         void renderVideo(payload)
     );
     await client.current.leave().catch((e) => console.log("leave error", e));
-    // hard refresh to reset the state
+    // hard refresh to clear the state
     window.location.href = "/";
-    // setInCall(false);
   };
 
   return (
@@ -88,14 +83,17 @@ const Videocall = (props: { slug: string; JWT: string }) => {
       <h1 className="text-center text-3xl font-bold mb-4 mt-0">
         Session: {session}
       </h1>
-      <div className="flex w-full flex-1" style={inCall ? {} : { display: "none" }}>
+      <div
+        className="flex w-full flex-1"
+        style={inSession ? {} : { display: "none" }}
+      >
         {/* @ts-expect-error html component */}
         <video-player-container ref={videoContainerRef} style={videoPlayerStyle} />
       </div>
-      {!inCall ? (
+      {!inSession ? (
         <div className="mx-auto flex w-64 flex-col self-center">
           <div className="w-4" />
-          <Button className="flex flex-1" onClick={startCall}>
+          <Button className="flex flex-1" onClick={joinSession} title="join session">
             Join
           </Button>
         </div>
@@ -113,7 +111,7 @@ const Videocall = (props: { slug: string; JWT: string }) => {
               client={client}
               setIsAudioMuted={setIsAudioMuted}
             />
-            <Button onClick={leaveCall} title="leave call">
+            <Button onClick={leaveSession} title="leave session">
               <PhoneOff />
             </Button>
           </div>
